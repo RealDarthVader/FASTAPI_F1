72b0baf34461efc285fa98492e445fa6a6f17eb1
Utkarsh Gupta <utkarshgupta.vk@gmail.com>
2026-08-16 17:07:15 +0530

    Fix vercel rewrites and api handler

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
-# api/index.py
 import sys
 import os
 
-# Add the Backend folder to Python's module search path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Backend")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Backend")))
 
 from app.main import app  # type: ignore
